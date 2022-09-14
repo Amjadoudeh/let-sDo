955c37a014064cf705f1ad0d43b900f8f5ff3d7c
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-14 10:34:11 +0200

    First commit

---
+++ Let'sDo/Let_sDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_sDoApp.swift
+//  Let'sDo
+//
+//  Created by Amjad Oudeh on 14.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Let_sDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
