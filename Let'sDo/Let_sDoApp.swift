aa9b48b2b29f2ab9e0de5f978c35b9bfe6e0a603
Amjad Oudeh <74506206+Amjadoudeh@users.noreply.github.com>
2022-09-14 10:44:51 +0200

    Preapar the Folders and wrap the app in NavigationView

--- Let'sDo/Let_sDoApp.swift
+++ Let'sDo/Let_sDoApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct Let_sDoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ListView()
+            }
         }
     }
 }
